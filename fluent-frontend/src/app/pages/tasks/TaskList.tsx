bec64857eccabb90ed80e7ac66ed8665ba801308
Mohamed Rhoulam <mohamedrhoulam.55@gmail.com>
2024-12-11 22:31:41 +0100

    udpate tasklist

--- fluent-frontend/src/app/pages/tasks/TaskList.tsx
+++ fluent-frontend/src/app/pages/tasks/TaskList.tsx
@@ -11,6 +11,7 @@ import TaskSidebar from "./TaskSidebar";
 const TaskList: React.FC = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [expandedTaskId, setExpandedTaskId] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -37,6 +38,10 @@ const TaskList: React.FC = () => {
     setTasks((prevTasks) =>
       prevTasks.map((t) => (t._id === task._id ? updatedTask : t))
     );
+  };
+
+  const handleToggleExpand = (taskId: string) => {
+    setExpandedTaskId((prevId) => (prevId === taskId ? null : taskId));
   };
 
   return (
@@ -70,62 +75,123 @@ const TaskList: React.FC = () => {
 
             <tbody className="divide-y divide-gray-40">
               {tasks.map((task) => (
-                <tr key={task._id}>
-                  <td
-                    className="px-8 py-9 whitespace-nowrap overflow-hidden overflow-ellipsis text-sm font-medium flex justify-between items-center"
-                    style={{ maxWidth: "100%" }}
-                  >
-                    <span
-                      className={
-                        task.completed ? "line-through text-gray-500" : ""
-                      }
+                <React.Fragment key={task._id}>
+                  <tr onClick={() => handleToggleExpand(task._id)}>
+                    <td
+                      className="px-8 py-9 whitespace-nowrap overflow-hidden overflow-ellipsis text-sm font-medium flex justify-between items-center cursor-pointer"
+                      style={{ maxWidth: "100%" }}
                     >
-                      {task.title}
-                    </span>
-                    <div className="flex space-x-2">
-                      <Badge
-                        variant="secondary"
-                        className="cursor-pointer"
-                        onClick={() => handleToggleCompleted(task)}
+                      <span
+                        className={`text-lg ${
+                          task.completed ? "line-through text-gray-500" : ""
+                        }`}
                       >
-                        {task.completed ? "Completed" : "Undue"}
-                      </Badge>
-                      <Badge
-                        variant="secondary"
-                        className="py-2 cursor-pointer"
-                        onClick={() => handleToggleCompleted(task)}
-                      >
-                        {task.dueDate
-                          ? format(new Date(task.dueDate), "MMM dd, yy")
-                          : "No due date"}
-                      </Badge>
-                      {task.dueDate && isToday(new Date(task.dueDate)) && (
+                        {task.title}
+                      </span>
+                      <div className="flex space-x-2">
                         <Badge
                           variant="secondary"
-                          className="bg-red-200 text-red-800 cursor-pointer"
+                          className="cursor-pointer"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            handleToggleCompleted(task);
+                          }}
                         >
-                          Due Today
+                          {task.completed ? "Completed" : "Undue"}
                         </Badge>
-                      )}
-                      {task.dueDate && isTomorrow(new Date(task.dueDate)) && (
                         <Badge
                           variant="secondary"
-                          className="bg-purple-200 text-purple-800"
+                          className="py-2 cursor-pointer"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            handleToggleCompleted(task);
+                          }}
                         >
-                          Due Tomorrow
+                          {task.dueDate
+                            ? format(new Date(task.dueDate), "MMM dd, yy")
+                            : "No due date"}
                         </Badge>
-                      )}
-                      {task.dueDate &&
-                        isThisWeek(new Date(task.dueDate)) &&
-                        !isToday(new Date(task.dueDate)) &&
-                        !isTomorrow(new Date(task.dueDate)) && (
-                          <Badge variant="secondary" className="border ">
-                            Due This Week
+                        {task.dueDate && isToday(new Date(task.dueDate)) && (
+                          <Badge
+                            variant="secondary"
+                            className="bg-red-200 text-red-800 cursor-pointer"
+                          >
+                            Due Today
                           </Badge>
                         )}
-                    </div>
-                  </td>
-                </tr>
+                        {task.dueDate && isTomorrow(new Date(task.dueDate)) && (
+                          <Badge
+                            variant="secondary"
+                            className="bg-purple-200 text-purple-800 cursor-pointer"
+                          >
+                            Due Tomorrow
+                          </Badge>
+                        )}
+                        {task.dueDate &&
+                          isThisWeek(new Date(task.dueDate)) &&
+                          !isToday(new Date(task.dueDate)) &&
+                          !isTomorrow(new Date(task.dueDate)) && (
+                            <Badge
+                              variant="secondary"
+                              className="border border-gray-400 text-gray-800 cursor-pointer"
+                            >
+                              Due This Week
+                            </Badge>
+                          )}
+                      </div>
+                    </td>
+                  </tr>
+                  {expandedTaskId === task._id && (
+                    <tr>
+                      <td className="px-8 py-4 text-sm text-gray-700">
+                        <div>
+                          Description: {task.description || "No description"}
+                        </div>
+                        <div>Location: {task.location || "No location"}</div>
+                        <div>
+                          Participants:{" "}
+                          {task.participants.join(", ") || "No participants"}
+                        </div>
+                        <div>
+                          Subtasks:{" "}
+                          {task.subtasks.length > 0
+                            ? task.subtasks.map((subtask) => (
+                                <div key={subtask._id}>
+                                  <div>Title: {subtask.title}</div>
+                                  <div>
+                                    Description:{" "}
+                                    {subtask.description || "No description"}
+                                  </div>
+                                  <div>
+                                    Completed:{" "}
+                                    {subtask.completed ? "Yes" : "No"}
+                                  </div>
+                                  <div>
+                                    Due Date:{" "}
+                                    {subtask.dueDate
+                                      ? format(
+                                          new Date(subtask.dueDate),
+                                          "MMM dd, yy"
+                                        )
+                                      : "No due date"}
+                                  </div>
+                                  <div>
+                                    Location:{" "}
+                                    {subtask.location || "No location"}
+                                  </div>
+                                  <div>
+                                    Participants:{" "}
+                                    {subtask.participants.join(", ") ||
+                                      "No participants"}
+                                  </div>
+                                </div>
+                              ))
+                            : "No subtasks"}
+                        </div>
+                      </td>
+                    </tr>
+                  )}
+                </React.Fragment>
               ))}
             </tbody>
           </table>
