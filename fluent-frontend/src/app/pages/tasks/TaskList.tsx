ee0c7b52743f19dd7d7e165eba3fca6c02111553
Mohamed Rhoulam <mohamedrhoulam.55@gmail.com>
2024-12-11 20:14:18 +0100

    update task list

--- fluent-frontend/src/app/pages/tasks/TaskList.tsx
+++ fluent-frontend/src/app/pages/tasks/TaskList.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import { Button } from "../../components/ui/button"; // Adjust the import path as needed
 import { fetchTasks } from "../../services/taskService";
 import { Task } from "../../types/Task";
 
@@ -23,32 +24,37 @@ const TaskList: React.FC = () => {
 
   return (
     <div className="w-1/2 mx-auto pt-4">
-    <div className="max-h-[500px] overflow-y-auto rounded-lg border shadow-inner">
-      <table className="w-full table-fixed divide-y divide-gray-40">
-        <thead>
-        <tr>
-          <th
-            scope="col"
-            className="px-8 py-4 text-left text-lg font-medium tracking-wider text-gray-500"
-          >
-            Tasks
-          </th>
-        </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-40">
-        {tasks.map((task) => (
-          <tr key={task._id}>
-            <td
-            className="px-8 py-9 whitespace-nowrap overflow-hidden overflow-ellipsis text-sm font-medium"
-            style={{ maxWidth: "100%" }}
-            >
-            {task.title}
-            </td>
-          </tr>
-        ))}
-        </tbody>
-      </table>
-    </div>
+      <div className="max-h-[500px] overflow-y-auto rounded-lg border shadow-inner">
+        <table className="w-full table-fixed divide-y divide-gray-40">
+          <thead>
+            <tr>
+              <th
+                scope="col"
+                className="px-8 py-4 text-left text-lg font-medium tracking-wider text-gray-500"
+              >
+                <div className="flex justify-between items-center">
+                  <span>Tasks</span>
+                  <Button variant="default" size="sm">
+                    Add Task
+                  </Button>
+                </div>
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-40">
+            {tasks.map((task) => (
+              <tr key={task._id}>
+                <td
+                  className="px-8 py-9 whitespace-nowrap overflow-hidden overflow-ellipsis text-sm font-medium"
+                  style={{ maxWidth: "100%" }}
+                >
+                  {task.title}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
