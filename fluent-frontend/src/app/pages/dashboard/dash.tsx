42824215708f673229b6c3a36991c332ca69a417
Mohamed Rhoulam <mohamedrhoulam.55@gmail.com>
2024-12-12 01:05:50 +0100

    add today's list

--- fluent-frontend/src/app/pages/dashboard/dash.tsx
+++ fluent-frontend/src/app/pages/dashboard/dash.tsx
@@ -31,6 +31,16 @@ import {
   ChartTooltipContent,
 } from "../../components/ui/chart";
 import { fetchTasks } from "../../services/taskService";
+import { Badge } from "../../components/ui/badge";
+import { Pencil } from "lucide-react";
+import { format, isToday } from "date-fns";
+
+type Task = {
+  _id: string;
+  title: string;
+  dueDate?: Date;
+  completed: boolean;
+};
 
 const Dashboard = () => {
   const [chartData, setChartData] = useState<
@@ -40,6 +50,7 @@ const Dashboard = () => {
     { name: string; value: number; fill: string }[]
   >([]);
   const [todayTasksCount, setTodayTasksCount] = useState(0);
+  const [todayTasks, setTodayTasks] = useState<Task[]>([]);
 
   useEffect(() => {
     const getTasks = async () => {
@@ -99,6 +110,13 @@ const Dashboard = () => {
           today.toLocaleDateString("en-US", { weekday: "short" })
       );
       setTodayTasksCount(todayTasks ? todayTasks.count : 0);
+
+      const filteredTodayTasks = tasks.filter((task) => {
+        if (!task.dueDate) return false;
+        const dueDate = new Date(task.dueDate);
+        return isToday(dueDate);
+      });
+      setTodayTasks(filteredTodayTasks);
     };
 
     getTasks();
@@ -118,14 +136,14 @@ const Dashboard = () => {
   } satisfies ChartConfig;
 
   return (
-    <div>
+    <div className="dashboard flex-row">
       <div className="greeting font-geist font-bold text-3xl pb-4 pl-2">
         {" "}
         Hello, you have {todayTasksCount} things to do for today{" "}
       </div>
-      <Card className="flex flex-row">
-        <div className="flex flex-row">
-          <div className="flex-1 border rounded-lg p-4">
+      <Card className=" charts flex justify-center">
+        <div className="flex">
+          <div className="flex-1 rounded-lg p-4">
             <CardHeader>
               <CardTitle>Tasks for the Next 7 Days</CardTitle>
               <CardDescription>
@@ -180,7 +198,7 @@ const Dashboard = () => {
               </div>
             </CardFooter>
           </div>
-          <div className="flex-1 border rounded-lg p-4">
+          <div className="flex-1  rounded-lg p-4">
             <CardHeader>
               <CardTitle>All Time Task Completion</CardTitle>
               <CardDescription>
@@ -254,7 +272,7 @@ const Dashboard = () => {
               </ChartContainer>
             </CardContent>
           </div>
-          <div className="flex-1 border rounded-lg p-4">
+          <div className="flex-1  rounded-lg p-4">
             <CardHeader>
               <CardTitle>Tasks Distribution</CardTitle>
               <CardDescription>
@@ -317,6 +335,66 @@ const Dashboard = () => {
           </div>
         </div>
       </Card>
+      <div className=" todays-tasks mt-8 flex justify-center">
+        <div className="w-1/2 mx-auto pt-4 transition-transform duration-300">
+          <div className="max-h-[500px] overflow-y-auto rounded-lg border shadow-inner">
+            <table className="w-full table-fixed divide-y divide-gray-40">
+              <thead className="sticky top-0 z-10">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-8 py-4 text-left text-lg font-medium tracking-wider text-gray-500 bg-[#09090b]"
+                  >
+                    <div className="flex justify-between items-center ">
+                      <span>Today's Tasks</span>
+                    </div>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-40">
+                {todayTasks.map((task) => (
+                  <tr key={task._id} className="relative group">
+                    <td
+                      className="px-8 py-9 whitespace-nowrap overflow-hidden overflow-ellipsis text-sm font-medium flex justify-between items-center cursor-pointer"
+                      style={{ maxWidth: "100%" }}
+                    >
+                      <span
+                        className={`text-lg ${
+                          task.completed ? "line-through text-gray-500" : ""
+                        }`}
+                      >
+                        {task.title}
+                      </span>
+                      <div className="flex space-x-2">
+                        <Badge variant="secondary" className="cursor-pointer">
+                          {task.completed ? "Completed" : "Undue"}
+                        </Badge>
+                        <Badge
+                          variant="secondary"
+                          className="py-2 cursor-pointer"
+                        >
+                          {task.dueDate
+                            ? format(new Date(task.dueDate), "MMM dd, yy")
+                            : "No due date"}
+                        </Badge>
+                        {task.dueDate && isToday(new Date(task.dueDate)) && (
+                          <Badge
+                            variant="secondary"
+                            className="bg-red-200 text-red-800 cursor-pointer"
+                          >
+                            Due Today
+                          </Badge>
+                        )}
+                      </div>
+                      <Pencil className="absolute right-2 top-1/2 transform -translate-y-1/2 opacity-0 group-hover:opacity-50 group-hover:opacity-100 cursor-pointer h-3 w-3" />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
